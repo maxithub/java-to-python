4dd324b8e36b91925aeea92d4b7cd9f9afab0d75
Max Yuan <max.m.yuan@gmail.com>
2018-12-04 23:24:39 +0800

    first commit

---
+++ p01-basic.py
@@ -0,0 +1,21 @@
+# ------------------------- Hello World ----------------------------
+print("Hello, World")
+
+# ------------------------- Big syntax differences ----------------------------
+
+# 01 No need ; for ending lines
+message = "Hello World"
+
+
+# 02 No { }, use indentation and :
+def say_hello(name):
+    if name:
+        print("Hello " + name)
+    else:
+        print("What is your name?")
+
+
+# 03
+
+# Data types
+# number
